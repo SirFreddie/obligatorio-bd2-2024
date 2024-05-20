0dae6278714d99dead7ed48b732f96704e649d54
dana2002 <danacastroman@gmail.com>
2024-05-19 21:11:52 -0300

    db-connection

--- obligatorio-bd2-2024-server/src/app.ts
+++ obligatorio-bd2-2024-server/src/app.ts
@@ -1,8 +1,20 @@
 import dotenv from 'dotenv';
 import Server from './models/server.model';
+import { pool } from './db/config';
 
 dotenv.config();
 
 const server = new Server();
 
+const getUsers = async () => {
+	try {
+		const result = await pool.query('SELECT * FROM USER');
+		console.log(result);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
 server.listen();
+
+getUsers();
