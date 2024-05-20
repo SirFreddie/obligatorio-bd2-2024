641af31d3e5dc48a304b60bce4490df0461d5688
dana2002 <danacastroman@gmail.com>
2024-05-19 21:11:52 -0300

    db-connection

---
+++ obligatorio-bd2-2024-server/src/db/config.ts
@@ -0,0 +1,12 @@
+import { createPool } from 'mysql2/promise';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const pool = createPool({
+	host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT!), 
+	multipleStatements: true
+});
