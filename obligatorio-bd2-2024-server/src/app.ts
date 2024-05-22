0f124f51764ba50eea8692a97a85028be2d24c5e
dana2002 <danacastroman@gmail.com>
2024-05-22 01:01:02 -0300

    DB hosted in Docker

--- obligatorio-bd2-2024-server/src/app.ts
+++ obligatorio-bd2-2024-server/src/app.ts
@@ -1,14 +1,17 @@
 import dotenv from 'dotenv';
 import Server from './models/server.model';
 import { pool } from './db/config';
+import express from 'express';
 
 dotenv.config();
 
 const server = new Server();
 
+const app = express();
+
 const getUsers = async () => {
 	try {
-		const result = await pool.query('SELECT * FROM USER');
+		const result = await pool.query('SELECT * FROM user');
 		console.log(result);
 	} catch (error) {
 		console.error(error);
--- obligatorio-bd2-2024-server/src/db/config.ts
+++ obligatorio-bd2-2024-server/src/db/config.ts
@@ -4,9 +4,9 @@ dotenv.config();
 
 export const pool = createPool({
 	host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT!), 
-	multipleStatements: true
+	database: process.env.DB_NAME,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	port: parseInt(process.env.DB_PORT!, 10),
+	multipleStatements: true,
 });
