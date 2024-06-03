ac6ead7afcdbcb3bede908bc7256684857fddbfa
Cdogonza <gonzalo.paz@correo.ucu.edu.uy>
2024-06-03 00:20:36 -0300

    logica parcial del registro de usuario

--- obligatorio-bd2-2024-client/src/app/core/models/interfaces/IUser.interface.ts
+++ obligatorio-bd2-2024-client/src/app/core/models/interfaces/IUser.interface.ts
@@ -1,10 +1,13 @@
 export interface IUser {
-  id: number;
+  user_id: number;
   name: string;
   surname: string;
   email: string;
+  password?: string;
 }
 
 export interface IStudent extends IUser {
   points: number;
+  campeon: string;
+  subcampeon: string;
 }
--- obligatorio-bd2-2024-client/src/app/modules/auth/pages/register/register.component.ts
+++ obligatorio-bd2-2024-client/src/app/modules/auth/pages/register/register.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RegistroService } from '../../../../core/services/registro.service';
+import { IUser } from '../../../../core/models/interfaces/IUser.interface';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export default class RegisterComponent {}
+export default class RegisterComponent {
+
+  formulario: FormGroup;
+  constructor(private registroService: RegistroService) { 
+
+    this.formulario = new FormGroup({
+      nombre: new FormControl(),
+      apellido: new FormControl(),
+      cedula: new FormControl(),
+      email: new FormControl(),
+      password: new FormControl(),
+      campeon: new FormControl(),
+      subcampeon: new FormControl()
+    });
+  }
+
+  onSubmit() {
+    if (this.formulario?.valid) {
+
+    const usuario:IUser = {
+      nombre: this.formulario.value.nombre,
+      apellido: this.formulario.value.apellido,
+      cedula: this.formulario.value.cedula,
+      email: this.formulario.value.email,
+      password: this.formulario.value.password,
+      campeon: this.formulario.value.campeon,
+      subcampeon: this.formulario.value.subcampeon
+    };
+      this.registroService.registrarUsuario(usuario).toPromise().then(() => {
+        console.log('Usuario registrado');
+        this.limpiar();
+        
+      }).catch((error) => {
+        console.error('Error al registrar usuario', error);
+      });
+    }
+  }
+  limpiar() {
+    this.formulario?.reset();
+
+  }
+
+}
