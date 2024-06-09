d8c0f54eae17aa962ff6110b620f0f71222c1612
dana2002 <danacastroman@gmail.com>
2024-06-09 02:10:50 -0300

    Jwt and login

--- obligatorio-bd2-2024-server/src/routes/user.routes.ts
+++ obligatorio-bd2-2024-server/src/routes/user.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
-import { createUser, getPoints } from '../controllers/user.controller';
+import {
+	createUser,
+	getPoints,
+	isValidToken,
+	loginUser,
+} from '../controllers/user.controller';
 
 const router = Router();
 
 router.post('/new', createUser);
 router.get('/points', getPoints);
+router.post('/login', loginUser);
+router.get('/isValidToken', isValidToken)
 
 export default router;
